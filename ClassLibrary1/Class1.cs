78762f531a391d76dc19295aeed63fb1ae97d8e8
Андрей Морозов <am321m3@list.ru>
2023-02-20 14:59:51 +0300

    deleted comment

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -28,8 +28,6 @@ namespace ClassLibrary1
             }
         }
 
-        //0 1 2 3 4 5 6 7 8 9 A B C D E F G H J K L M N P R S T U V W X Y Z
-
         public static string GetVINCountry(string vin)
         {
             try
